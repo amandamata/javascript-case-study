8d98acfa197fb0d20b672dd718831f988e2ba1b6
Amanda Mata <amanda.mata@live.com>
2021-01-14 16:12:56 -0300

    class negotiation
    private attbutes, defensive programming on date, immutability

---
+++ client/js/app/models/Negotiation.js
@@ -0,0 +1,25 @@
+class Negotiation {
+    
+    constructor(date, quantity, price){
+        this._date = new Date(date.getTime());
+        this._quantity = quantity;
+        this._price = price;
+        Object.freeze(this);
+    }
+
+    get volume() {
+        return this._quantity * this._price;
+    }
+
+    get date(){
+        return new Date(this._date.getTime());
+    }
+
+    get quantity(){
+        return this._quantity;
+    }
+
+    get price(){
+        return this._price;
+    }
+} 
