da354fee485893acc40b214d590d3c461b8b5bed
Amanda Mata <amanda.mata@live.com>
2021-01-15 19:27:57 -0300

    negotiations methods

--- client/js/app/controllers/NegotiationController.js
+++ client/js/app/controllers/NegotiationController.js
@@ -5,15 +5,27 @@ class NegotiationController {
         this._inputDate = $('#date');
         this._inputQuantity = $('#quantity');
         this._inputPrice = $('#price');
+        this._negotiationsList = new NegotiationsList();
     }
 
     add(event){
         event.preventDefault();
+        this._negotiationsList.add(this._createNegotiation());
+        this._clearForm();
+    }
 
-        let negotiation = new Negotiation(
+    _createNegotiation(){
+        return new Negotiation(
             DateHelper.transformDate(this._inputDate.value), 
             this._inputQuantity.value,
             this._inputPrice.value
         );
+    }
+
+    _clearForm(){
+        this._inputDate.value = '';
+        this._inputQuantity.value = 1;
+        this._inputPrice.value = 0.0;
+        this._inputDate.focus();
     }
 }
---
+++ client/js/app/models/NegotiationsList.js
@@ -0,0 +1,13 @@
+class NegotiationsList {
+    constructor(){
+        this._negotiations = [];
+    }
+
+    add(negotiation){
+        this._negotiations.push(negotiation);
+    }
+
+    get negotiations(){
+        return [].concat(this._negotiations);
+    }
+}
