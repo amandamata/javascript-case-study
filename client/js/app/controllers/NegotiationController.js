690386df98b352fd8ae62d15487ec9f6828ef5a4
Amanda Mata <amanda.mata@live.com>
2021-01-16 11:20:07 -0300

    negotiations view

--- client/js/app/controllers/NegotiationController.js
+++ client/js/app/controllers/NegotiationController.js
@@ -6,11 +6,14 @@ class NegotiationController {
         this._inputQuantity = $('#quantity');
         this._inputPrice = $('#price');
         this._negotiationsList = new NegotiationsList();
+        this._negotiationsView = new NegotiationsView($('#negotiationsView'));
+        this._negotiationsView.update(this._negotiationsList);
     }
 
     add(event){
         event.preventDefault();
         this._negotiationsList.add(this._createNegotiation());
+        this._negotiationsView.update(this._negotiationsList);
         this._clearForm();
     }
 
---
+++ client/js/app/views/NegotiationsView.js
@@ -0,0 +1,44 @@
+class NegotiationsView{
+    
+    constructor(element){
+        this._element = element;
+    }
+
+    _template(model){
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            
+            <tbody> 
+                ${model.negotiations.map(n =>`
+
+                        <tr>
+                            <td>${DateHelper.showDate(n.date)}</td>
+                            <td>${n.quantity}</td>
+                            <td>${n.price}</td>
+                            <td>${n.volume}</td>
+                        </tr>
+
+                `).join('')}
+            </tbody>
+            
+            <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.negotiations.reduce((total, n) => total += n.volume, 0.0)}
+                </td>
+            </tfoot>
+        </table>`;
+    }
+
+    update(model){
+        this._element.innerHTML = this._template(model);
+    }
+}
